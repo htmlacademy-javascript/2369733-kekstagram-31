b6426cbcdad467a5b35404891d857aaa1d6d2db5
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-02-20 20:31:50 +0300

    i+

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,7 @@ const testPolyndrome = (string = '') => {
   string = string.replaceAll(' ', '').toLowerCase();
   let reversal = '';
 
-  for (let i = string.length - 1; I >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     reversal += string[1];
   }
   return string === reversal;
