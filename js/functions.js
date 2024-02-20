dfc3fa420f55af2f95aa6d7a1350fd8c0a7653c0
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-02-20 20:24:07 +0300

    палиндромом+checkStringLength

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,17 @@
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
+const checkStringLength = (string = '', maxSymbols = 1) => string.length <= maxSymbols;
 
-checkStringLength ('nhb', 3);
+checkStringLength('проверяемая строка', 20);
+
+
+const testPolyndrome = (string = '') => {
+  string = string.replaceAll(' ', '').toLowerCase();
+  let reversal = '';
+
+  for (let i = string.length - 1; I >= 0; i--) {
+    reversal += string[1];
+  }
+  return string === reversal;
+};
+
+
+testPolyndrome('Лёша на полке клопа нашёл ');
