81037fa4e72787842f1989ca4be93d7f385fb7df
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-02-23 13:30:26 +0300

    extractNumber

--- js/functions.js
+++ js/functions.js
@@ -15,3 +15,21 @@ const testPolyndrome = (string = '') => {
 
 
 testPolyndrome('Лёша на полке клопа нашёл ');
+
+
+const extractNumber = (string) => {
+  let result = '';
+
+  string = string.toString();
+
+  for (let i = 0; i <= string.length; i++) {
+    if (Number.isNaN(parseInt(string[i] , 10)) === false){
+      result += string[i];
+    }
+  }
+  return result === '' ? NaN : Number(result);
+};
+
+
+extractNumber('извлекает содержащиеся в ней цифры от 0 до 9');
+
