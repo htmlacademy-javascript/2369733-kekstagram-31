61904780f166162e2d3aa721751e72d28cfa69ff
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-03-01 20:11:05 +0300

    console.log

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ import {
   testPolyndrome
 } from './test-polyndrome';
 
-console.log(2 + 2);
-console.log(checkStringLength('проверяемая строка', 20));
-console.log(extractNumber('извлекает содержащиеся в ней цифры от 0 до 9'));
-console.log(testPolyndrome('Лёша на полке клопа нашёл '));
-console.table(generatedObjectArrays());
+
+// console.log(checkStringLength('проверяемая строка', 20));
+// console.log(extractNumber('извлекает содержащиеся в ней цифры от 0 до 9'));
+// console.log(testPolyndrome('Лёша на полке клопа нашёл '));
+// console.table(generatedObjectArrays());
