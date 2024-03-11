9d346a4101064b69049d991ae272045b1a962681
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-03-11 21:57:26 +0300

    showBigPicture

--- js/components/render-pictures.js
+++ js/components/render-pictures.js
@@ -1,20 +1,22 @@
+import { showBigPicture } from './show-big-picture';
+
 const pictureTemplate = document
   .querySelector('#picture')
   .content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 
-const createPicture = ({
-  comments,
-  description,
-  likes,
-  url
-}) => {
+const createPicture = (data) => {
+  const { comments, description, likes, url } = data;
   const picture = pictureTemplate.cloneNode(true);
 
   picture.querySelector('.picture__img').src = url;
   picture.querySelector('.picture__img').alt = description;
   picture.querySelector('.picture__comments').textContent = comments.length;
   picture.querySelector('.picture__likes').textContent = likes;
+
+  picture.addEventListener('click', () => {
+    showBigPicture(data);
+  });
 
   return picture;
 };
@@ -29,6 +31,4 @@ const renderPictures = (pictures) => {
   container.append(fragment);
 };
 
-export {
-  renderPictures
-};
+export { renderPictures };
